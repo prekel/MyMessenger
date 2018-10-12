738ef1c979cc1ce406dc17984df2be043978752e
Vladislav Prekel <misterptits@yandex.ru>
2018-10-13 00:49:38 +0700

    begins work

--- MyMessenger.Server/Class1.cs
+++ MyMessenger.Server/Class1.cs
@@ -1,9 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Net;
 using System.Text;
 using Microsoft.EntityFrameworkCore;
 using MySql.Data.EntityFrameworkCore;
 using MySql.Data.EntityFrameworkCore.Extensions;
+using MySql.Data.MySqlClient;
+using Renci.SshNet;
+using Renci.SshNet.Common;
 
 namespace MyMessenger.Server
 {
@@ -31,7 +36,7 @@ namespace MyMessenger.Server
 
 		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 		{
-			optionsBuilder.UseMySQL("server=51.158.73.185;database=TestDB;user=root;password=");
+			optionsBuilder.UseMySQL("server=51.158.73.185;database=TestDB;user=vladislav;password=;SslMode=none");
 		}
 
 		protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -57,8 +62,60 @@ namespace MyMessenger.Server
 	{
 		public static void Main1(string[] args)
 		{
-			InsertData();
-			PrintData();
+						InsertData();
+						PrintData();
+
+			//var passwd = Console.ReadLine();
+			//var connectionInfo = new PasswordConnectionInfo("51.158.73.185", "vladislav", passwd);
+			//connectionInfo.Timeout = TimeSpan.FromSeconds(30);
+
+			//using (var client = new SshClient(connectionInfo))
+			//{
+			//	try
+			//	{
+			//		Console.WriteLine("Trying SSH connection...");
+			//		client.Connect();
+			//		if (client.IsConnected)
+			//		{
+			//			Console.WriteLine("SSH connection is active: {0}", client.ConnectionInfo.ToString());
+			//		}
+			//		else
+			//		{
+			//			Console.WriteLine("SSH connection has failed: {0}", client.ConnectionInfo.ToString());
+			//		}
+
+			//		Console.WriteLine("\r\nTrying port forwarding...");
+			//		var portFwld = new ForwardedPortLocal(IPAddress.Loopback.ToString(), 22, "localhost", 3305);
+			//		client.AddForwardedPort(portFwld);
+			//		portFwld.Start();
+			//		if (portFwld.IsStarted)
+			//		{
+			//			Console.WriteLine("Port forwarded: {0}", portFwld.ToString());
+			//		}
+			//		else
+			//		{
+			//			Console.WriteLine("Port forwarding has failed.");
+			//		}
+
+			//	}
+			//	catch (SshException e)
+			//	{
+			//		Console.WriteLine("SSH client connection error: {0}", e.Message);
+			//	}
+			//	catch (System.Net.Sockets.SocketException e)
+			//	{
+			//		Console.WriteLine("Socket connection error: {0}", e.Message);
+			//	}
+
+			//}
+
+			//Console.WriteLine("\r\nTrying database connection...");
+			//DBConnect dbConnect = new DBConnect("localhost", "test_database", "root", "passwrod123", "4479");
+
+			//var ct = dbConnect.Count("packages");
+			//Console.WriteLine(ct.ToString());
+
+
 		}
 
 		private static void InsertData()
