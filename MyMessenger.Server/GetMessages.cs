a3e8bd5fe1467965b82a05105241a10121f8c718
Vladislav Prekel <misterptits@yandex.ru>
2018-10-31 09:19:31 +0700

    GetMessages

--- MyMessenger.Server/GetMessages.cs
+++ MyMessenger.Server/GetMessages.cs
@@ -1,12 +1,31 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using MyMessenger.Server.Entities;
 
 namespace MyMessenger.Server
 {
 	public class GetMessages : ICommand
 	{
+		private Query Config { get; set; }
+		private MessengerContext Context { get; set; }
+
+		public IEnumerable<Message> Result { get; private set; }
+
+		public GetMessages(MessengerContext context, Query query)
+		{
+			Config = query;
+		}
+
 		public void Execute()
 		{
-			throw new NotImplementedException();
-		}	
+			Result = from i in Context.Messages where i.Dialog.ID == Config.DialogId select i;
+		}
+
+		public class Query
+		{
+			public int DialogId { get; set; }
+		}
 	}
 }
