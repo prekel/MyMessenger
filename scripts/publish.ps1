7537031aa261d86d745aa16305209f9ce0de4cf8
Vladislav Prekel <misterptits@yandex.ru>
2018-12-23 16:04:00 +0700

    publish для разных платформ

--- scripts/publish.ps1
+++ scripts/publish.ps1
@@ -1,1 +1,3 @@
-dotnet publish /p:AssemblyVersion="$env:APPVEYOR_BUILD_VERSION"
+dotnet publish -c Release /p:AssemblyVersion="$env:APPVEYOR_BUILD_VERSION"
+dotnet publish -c Release -r win-x64 /p:AssemblyVersion="$env:APPVEYOR_BUILD_VERSION"
+dotnet publish -c Release -r lunux-x64 /p:AssemblyVersion="$env:APPVEYOR_BUILD_VERSION"
