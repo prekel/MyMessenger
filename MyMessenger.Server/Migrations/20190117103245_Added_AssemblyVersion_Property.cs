6d34bd8d66cb1323e80c2bb7a198347f00462d53
Vladislav Prekel <misterptits@yandex.ru>
2019-01-17 17:49:50 +0700

    миграция

---
+++ MyMessenger.Server/Migrations/20190117103245_Added_AssemblyVersion_Property.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyMessenger.Server.Migrations
+{
+    public partial class Added_AssemblyVersion_Property : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AssemblyVersion",
+                table: "Launches",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AssemblyVersion",
+                table: "Launches");
+        }
+    }
+}
