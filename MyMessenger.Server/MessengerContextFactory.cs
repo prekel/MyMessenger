0576d8e18b9316ad113c946bf0df40b3e3e2d09a
Vladislav Prekel <misterptits@yandex.ru>
2019-01-17 14:31:57 +0700

    MessengerContextFactory
    (cherry picked from commit 0906713f59b106cca3c74baa9358d06ff8823b44)

---
+++ MyMessenger.Server/MessengerContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using MyMessenger.Server.Configs;
+
+namespace MyMessenger.Server
+{
+	public class MessengerContextFactory : IDesignTimeDbContextFactory<MessengerContext>
+	{
+		public MessengerContext CreateDbContext(string[] args)
+		{
+			var c = new Config
+			{
+				DbConfig = new DbConfig
+				{
+					Server = "localhost",
+					Port = 3306,
+					Name = "MyMessenger5",
+					User = "root",
+					SslMode = "none",
+					Password = "123456"
+				}
+			};
+			return new MessengerContext(c);
+		}
+	}
+}
