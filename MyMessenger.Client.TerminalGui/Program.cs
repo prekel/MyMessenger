39535983328a00bb93bf7a1495e8616148237133
Vladislav Prekel <misterptits@yandex.ru>
2019-01-25 02:01:34 +0700

    gtk-wpf-terminal.gui

---
+++ MyMessenger.Client.TerminalGui/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyMessenger.Client.TerminalGui
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
---
+++ MyMessenger.Client.XamarinForms/MyMessenger.Client.XamarinForms.GTK/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyMessenger.Client.XamarinForms.GTK
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
