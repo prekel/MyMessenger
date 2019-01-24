b0db64a60a1d51fc146ba6deb0d12f1e6417d217
Vladislav Prekel <misterptits@yandex.ru>
2019-01-24 21:51:27 +0700

    wpf

---
+++ MyMessenger.Client.Xamarin.Forms/MyMessenger.Client.Xamarin.Forms.WPF/FormsApplicationPageWorkaround.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMessenger.Client.Xamarin.Forms.WPF
+{
+	public class FormsApplicationPageWorkaround : global::Xamarin.Forms.Platform.WPF.FormsApplicationPage
+	{
+	}
+}
---
+++ MyMessenger.Client.Xamarin.Forms/MyMessenger.Client.Xamarin.Forms.WPF/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Platform.WPF;
+using MyMessenger.Client.Xamarin.Forms;
+
+namespace MyMessenger.Client.Xamarin.Forms.WPF
+{
+	public partial class MainWindow : FormsApplicationPageWorkaround
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			global::Xamarin.Forms.Forms.Init();
+			LoadApplication(new Forms.App());
+		}
+	}
+}
