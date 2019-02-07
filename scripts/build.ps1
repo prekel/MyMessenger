2c014a071758173388dca6d4ccb16fe3bca1ffcf
Vladislav Prekel <misterptits@yandex.ru>
2019-02-08 00:29:19 +0700

    logger

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,4 +1,4 @@
-dotnet build /p:AssemblyVersion="$env:APPVEYOR_BUILD_VERSION" /verbosity:minimal /m
+dotnet build /p:AssemblyVersion="$env:APPVEYOR_BUILD_VERSION" /verbosity:minimal /m /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
 if ($LASTEXITCODE -ne 0)
 {
 	Exit-AppveyorBuild
